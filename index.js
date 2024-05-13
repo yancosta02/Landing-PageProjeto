0b951610c9ff44e9d01164809cde87b90f78282b
Yan Costa <yan.costa02outlook.com>
2024-05-13 14:02:02 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,42 @@
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var setaDireita = window.document.getElementById("seta-direita")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+var Bruna = window.document.getElementById("Bruna")
+
+function RolarParaDireita() {
+    Leonardo.style = "display:none"
+    Bruna.style = "display:flex; width:"
+    setaEsquerda.style = "display:flex; margin-top: 120px;"
+    setaDireita.style = "display:none"
+}
+
+function RolarParaEsquerda() {
+    Leonardo.style = "display:flex"
+    Bruna.style = "display:none"
+    setaEsquerda.style = "display:none"
+    setaDireita.style = "display:flex; margin-top:125%;"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
